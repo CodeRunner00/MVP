f2067bb2c7bb8c7201f667df27eef200e42a95a8
Eric Forsell <efor202@gmail.com>
2017-01-23 23:34:15 -0500

    Added mongoose

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
  var express = require("express");
  var app = express();
  var path    = require("path");
- console.log('before static');
+  var mongoose = require('mongoose');
+  var bodyParser = require('body-parser')
+  app.use(bodyParser.json());
+
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+
+
   app.use(express.static(__dirname));
 
- app.get("/", function(req, res){
+  mongoose.connect('mongodb://ds127439.mlab.com:27439/hrr21-mvpdatabase', options);
+
+var conn = mongoose.connection;
+
+conn.on('error', console.error.bind(console, 'connection error:'));
+
+conn.once('open', function() {
+
+  console.log("connection success!");
+
+app.get("/", function(req, res){
   //res.redirect('/Users/EWF/Desktop/HRR/MVP/index.html');
   res.sendFile(path.join(__dirname+'/index.html'));
  });
@@ -13,3 +30,6 @@
  app.listen(port, function() {
    console.log("Listening on " + port);
  });
+
+});
+
