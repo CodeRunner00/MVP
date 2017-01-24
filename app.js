b35bbf740361e8c8678da60799bca72f153fab30
Eric Forsell <efor202@gmail.com>
2017-01-24 16:33:54 -0500

    Update

--- app.js
+++ app.js
@@ -17,17 +17,51 @@ var cypherApp = angular.module('cypher', ['ngRoute']);
 
 cypherApp.controller('CypherController',['$scope', '$http', function($scope, $http) {
 
+  var cypher = {};
+
+  function cypherStr(str) {
+    var result = "";
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    for(var i  =0 ; i < str.length; i++) {
+
+      if(str[i] === 'x') {
+        result = result +'a';
+      } else if (str[i] === 'y') {
+        result = result +'b';
+      } else if(str[i] === 'z') {
+        result = result + 'c';
+      } else {
+        result = result + alphabet[alphabet.indexOf(str[i])+3];
+      }
+
+    }
+
+    return result;
+  }
+
   $scope.cyphered = "cypheredV";
-  var addOne = function (cypher) {
-    return $http({
+  $scope.addString = function () {
+
+    cypher.uncyphered = $scope.uncyphered;
+    cypher.cyphered = cypherStr(cypher.uncyphered);
+    $scope.cyphered = cypher.cyphered;
+     $http({
       method: 'POST',
-      url: '/',
+      url: '/api/cyphers',
       data: cypher
     });
-  };
-  // $scope.addCypher() {
 
-  //   addOne($scope.cypher);
-  // };
+     var results = $http({
+      method: 'GET',
+      url: '/api/cyphers'
+    }).then(function(result){
+      console.log('result ', result);
+      $scope.cyphers = result.data;
+    });
+     console.log(results);
+
+  };
+
+
 
 }]);
